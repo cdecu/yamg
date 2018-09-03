4e9c610ddb976f3256804537dbf5498fa0c93431
rmx.cdecu <carlos@restomax.com>
2018-09-03 22:31:56 +0200

    Added Hieroglyphs

---
+++ src/environments/consts.ts
@@ -0,0 +1,11 @@
+export const consts = {
+  GameMode_Numbers        : 'Numbers',
+  GameMode_GreekLetters1  : 'GreekLetters1',
+  GameMode_GreekLetters2  : 'GreekLetters2',
+  GameMode_HebrewLetters1 : 'HebrewLetters1',
+  GameMode_HebrewLetters2 : 'HebrewLetters2',
+  GameMode_EgyptianHiero1 : 'EgyptianHiero1',
+  GameMode_EgyptianHiero2 : 'EgyptianHiero2',
+  GameMode_Phone_Numbers  : 'Phone Numbers',
+  GameMode_Contacts       : 'Contacts',
+};
